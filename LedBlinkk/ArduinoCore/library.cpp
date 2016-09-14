e9b235fdedc5c8c55c6683dec8c08d64cafd44ec
Jarmo P <jarmo.pesonen@edu.turkuamk.fi>
2016-09-14 14:04:15 +0300

    init

---
+++ LedBlinkk/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 14.9.2016 12.55.20
+ * Author : Ilmeister
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
